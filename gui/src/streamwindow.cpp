00af173bf2000b3cbfbcdf9032d95fa5a6039577
tezolinplayer <biduebob1995@gmail.com>
2026-02-08 16:40:51 -0300

    Update streamwindow.cpp

--- gui/src/streamwindow.cpp
+++ gui/src/streamwindow.cpp
@@ -9,28 +9,29 @@
 #include <QGroupBox>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
-#include <QSettings>
 #include <QKeyEvent>
 #include <QMouseEvent>
+#include <QSettings>
 
-// --- LINKER BRIDGE: CONEXÃO COMPLETA COM O MOTOR ---
+// --- LINKER BRIDGE: CONEXÃO COM O MOTOR DO CONTROLLER.C ---
 extern "C" {
-    int v_stage1 = 0, h_stage1 = 0;
-    int v_stage2 = 0, h_stage2 = 0;
-    int v_stage3 = 0, h_stage3 = 0;
-    int anti_dz_global = 0;
-    int sticky_power_global = 750;
-    int lock_power_global = 100;
-    int start_delay_global = 2;
-    bool sticky_aim_global = false;
-    bool rapid_fire_global = false;
+    int recoil_v_global = 16, recoil_h_global = 0;
+    int lock_power_global = 100, start_delay_global = 2, sticky_power_global = 750, anti_dz_global = 0;
+    bool sticky_aim_global = false, rapid_fire_global = false;
 }
 
-StreamWindow::StreamWindow(const StreamSessionConnectInfo &info, QWidget *parent) : QMainWindow(parent) {
+// FIX CONSTRUTOR: Agora passa a 'connect_info' corretamente para evitar erro de compilação
+StreamWindow::StreamWindow(const StreamSessionConnectInfo &connect_info, QWidget *parent) 
+    : QMainWindow(parent), connect_info(connect_info) 
+{
     setAttribute(Qt::WA_DeleteOnClose);
-    session = new StreamSession(info, this);
+    session = new StreamSession(connect_info, this);
     connect(session, &StreamSession::SessionQuit, this, &StreamWindow::SessionQuit);
     Init();
+}
+
+StreamWindow::~StreamWindow() {
+    if (session) session->Stop();
 }
 
 void StreamWindow::Init() {
@@ -38,50 +39,49 @@ void StreamWindow::Init() {
     central->setStyleSheet("background-color: #050505; color: #00FF41; font-family: 'Consolas'; font-weight: bold;");
     QVBoxLayout *mainLayout = new QVBoxLayout(central);
 
-    // 1. SELEÇÃO DE ARMA E SALVAR
-    QGroupBox *topBox = new QGroupBox("PERFIS DANIEL GHOST", this);
+    // 1. SELEÇÃO DE ARMA (VISUAL v4.6)
+    QGroupBox *topBox = new QGroupBox("SELEÇÃO DE ARMA", this);
+    topBox->setStyleSheet("border: 1px solid #00FF41; padding: 5px;");
     QHBoxLayout *topLay = new QHBoxLayout(topBox);
     QComboBox *combo = new QComboBox(this);
     combo->addItems({"M416", "BERYL", "SCAR-L", "GENERIC"});
-    QPushButton *btnSave = new QPushButton("SALVAR", this);
-    btnSave->setStyleSheet("background-color: #004400; border: 1px solid #00FF41;");
+    combo->setStyleSheet("background-color: #111; color: #00FF41; border: 1px solid #00FF41;");
+    QPushButton *btnSave = new QPushButton("SALVAR PERFIL", this);
+    btnSave->setStyleSheet("background-color: #004400; border: 1px solid #00FF41; padding: 5px;");
     topLay->addWidget(combo); topLay->addWidget(btnSave);
     mainLayout->addWidget(topBox);
 
-    // 2. SMART ACTIONS - 3 ESTÁGIOS (COM NÚMEROS 0-100)
-    auto addStage = [&](QString n, int *v, int *h) {
-        mainLayout->addWidget(new QLabel(n, this));
-        QHBoxLayout *hB = new QHBoxLayout();
-        QLabel *lV = new QLabel(QString("V: %1").arg(*v));
-        QSlider *sV = new QSlider(Qt::Horizontal); sV->setRange(0, 100); sV->setValue(*v);
-        connect(sV, &QSlider::valueChanged, [=](int val){ *v = val; lV->setText(QString("V: %1").arg(val)); });
-        
-        QLabel *lH = new QLabel(QString("H: %1").arg(*h));
-        QSlider *sH = new QSlider(Qt::Horizontal); sH->setRange(-100, 100); sH->setValue(*h);
-        connect(sH, &QSlider::valueChanged, [=](int val){ *h = val; lH->setText(QString("H: %1").arg(val)); });
-        
-        hB->addWidget(lV); hB->addWidget(sV); hB->addWidget(lH); hB->addWidget(sH);
-        mainLayout->addLayout(hB);
+    // 2. AJUSTES COM NÚMEROS (0-100)
+    auto addRow = [&](QString name, int min, int max, int *var) {
+        QLabel *lab = new QLabel(name + QString(": %1").arg(*var), this);
+        QSlider *sld = new QSlider(Qt::Horizontal, this);
+        sld->setRange(min, max); sld->setValue(*var);
+        connect(sld, &QSlider::valueChanged, [=](int val){ 
+            *var = val; 
+            lab->setText(name + QString(": %1").arg(val)); 
+        });
+        mainLayout->addWidget(lab); mainLayout->addWidget(sld);
     };
 
-    addStage("ESTÁGIO 1: KICK (0-300ms)", &v_stage1, &h_stage1);
-    addStage("ESTÁGIO 2: TRANSIÇÃO (300-800ms)", &v_stage2, &h_stage2);
-    addStage("ESTÁGIO 3: FINAL (800ms+)", &v_stage3, &h_stage3);
+    addRow("Recoil Vertical (Força)", 0, 100, &recoil_v_global);
+    addRow("Lock Power (Trava)", 0, 100, &lock_power_global);
+    addRow("Magnetismo (Sticky AA)", 0, 1500, &sticky_power_global);
 
-    // 3. AJUSTES GERAIS
-    auto addGlobal = [&](QString n, int min, int max, int *var) {
-        QLabel *l = new QLabel(n + QString(": %1").arg(*var), this);
-        QSlider *s = new QSlider(Qt::Horizontal, this); s->setRange(min, max); s->setValue(*var);
-        connect(s, &QSlider::valueChanged, [=](int v){ *var = v; l->setText(n + QString(": %1").arg(v)); });
-        mainLayout->addWidget(l); mainLayout->addWidget(s);
-    };
-    addGlobal("Magnetismo (Sticky)", 0, 1500, &sticky_power_global);
-    addGlobal("Start Delay", 0, 15, &start_delay_global);
-
-    // 4. MACROS
+    // 3. MACROS
+    QGroupBox *macroBox = new QGroupBox("MACROS", this);
+    QHBoxLayout *mLayout = new QHBoxLayout(macroBox);
     QCheckBox *cbS = new QCheckBox("STICKY AIM", this);
+    cbS->setChecked(sticky_aim_global);
     connect(cbS, &QCheckBox::toggled, [](bool c){ sticky_aim_global = c; });
-    mainLayout->addWidget(cbS);
+    mLayout->addWidget(cbS);
+    mainLayout->addWidget(macroBox);
+
+    // Lógica de Salvamento
+    connect(btnSave, &QPushButton::clicked, [=](){
+        QSettings s("GhostZen", "Profiles");
+        s.setValue(combo->currentText() + "/v", recoil_v_global);
+        s.setValue(combo->currentText() + "/lock", lock_power_global);
+    });
 
     setCentralWidget(central);
     setWindowTitle("DANIEL GHOST ZEN ELITE | v5.5");
@@ -90,7 +90,7 @@ void StreamWindow::Init() {
     session->Start();
 }
 
-// Funções obrigatórias para o Linker e para o personagem andar
+// FUNÇÕES OBRIGATÓRIAS PARA COMPILAR E PARA O PERSONAGEM ANDAR
 void StreamWindow::SessionQuit(ChiakiQuitReason, const QString&) { close(); }
 void StreamWindow::LoginPINRequested(bool) {}
 void StreamWindow::OnNewWebConnection() {}
@@ -103,4 +103,3 @@ void StreamWindow::moveEvent(QMoveEvent *e) { QMainWindow::moveEvent(e); }
 void StreamWindow::resizeEvent(QResizeEvent *e) { QMainWindow::resizeEvent(e); }
 void StreamWindow::changeEvent(QEvent *e) { QMainWindow::changeEvent(e); }
 void StreamWindow::closeEvent(QCloseEvent *e) { if(session) session->Stop(); }
-StreamWindow::~StreamWindow() { if (session) session->Stop(); }
